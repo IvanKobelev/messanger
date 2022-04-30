347bbfe49f07bff5329b217cdac5c067d30302fa
Ivan.Kobelev <kobelev97@gmail.com>
2022-04-30 22:44:17 +0300

    Main-service. Add models and initial migration

--- main-service/src/models.py
+++ main-service/src/models.py
@@ -1,5 +1,56 @@
 from datetime import datetime
 
-from sqlalchemy import Integer, DateTime, String, Text, Column
+from sqlalchemy import ForeignKey, Integer, Enum, DateTime, String, Text, Column
 
 from src.database import Base
+from src.enums import ServiceUserRole, CompanyUserRole, ProjectUserRole
+
+
+class User(Base):
+
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    company_id = Column(Integer, ForeignKey("company.id"))
+    company_role = Column(Enum(CompanyUserRole, values_callable=lambda enum: [e.value for e in enum]))
+    created_ts = Column(DateTime, default=datetime.utcnow, nullable=False)
+    email = Column(String(), nullable=False, unique=True)
+    first_name = Column(String(32), nullable=False)
+    login = Column(String(32), nullable=False, unique=True)
+    password = Column(Text, nullable=False)
+    second_name = Column(String(32), nullable=False)
+    service_role = Column(Enum(ServiceUserRole, values_callable=lambda enum: [e.value for e in enum]),
+                          default=ServiceUserRole.user, nullable=False)
+
+
+class Company(Base):
+
+    __tablename__ = "company"
+
+    id = Column(Integer, primary_key=True)
+    created_ts = Column(DateTime, default=datetime.utcnow, nullable=False)
+    description = Column(Text)
+    title = Column(String(32), nullable=False)
+
+
+class Project(Base):
+
+    __tablename__ = "project"
+
+    id = Column(Integer, primary_key=True)
+    company_id = Column(Integer, ForeignKey("company.id"), nullable=False)
+    created_ts = Column(DateTime, default=datetime.utcnow, nullable=False)
+    description = Column(Text)
+    owner_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    title = Column(String(32), nullable=False)
+
+
+class ProjectUser(Base):
+
+    __tablename__ = "project_user"
+
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey("project.id"), nullable=False)
+    role = Column(Enum(ProjectUserRole, values_callable=lambda enum: [e.value for e in enum]),
+                       default=ProjectUserRole.user, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
